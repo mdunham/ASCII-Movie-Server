85264d3b0bce7dfe87f0e5f99aaa4c7b87dd4fe7
Matthew Dunham <matt@matthewdunham.com>
2011-04-18 00:22:45 -0500

    Adding iPhone slide to unlock to contact form

--- js/master.init.js
+++ js/master.init.js
@@ -17,8 +17,53 @@ if (window.jQuery) {
 			e.preventDefault();
 			scrollToPage($(this).attr('href'));
 		});
-		
+
+		var nativePlaceholderSupport = (function(){
+			var i = document.createElement('input');
+			return ('placeholder' in i);
+		})();
+
+		if (nativePlaceholderSupport){
+			$('label').hide();
+		}
+
+		// Set the slider to be sliding
+		$("#unlock-slider").slider({
+			handle: "#unlock-handle",
+			animate:true,
+			slide: function(e,ui)
+			{
+				$("#slide-to-unlock").css("opacity", 1-(parseInt($("#unlock-handle").css("left"))/120));
+			},
+			stop: function(e,ui)
+			{
+				if($("#unlock-handle").position().left == 205)
+				{
+					unlock();
+				}
+				else
+				{
+					$("#unlock-handle").animate({left: 0}, 200 );
+					$("#slide-to-unlock").animate({opacity: 1}, 200 );
+				}
+			}
+			}
+		);
+
+		var unlock = function()
+		{
+			//$("#iphone-inside").animate({backgroundPosition: '0 40'}, 400, '', function()
+			//{
+			alert('unlock()');
+				$("#unlock-bottom").animate({bottom: -100}, 300);
+				$("#unlock-top").animate({top: -100}, 300, '', function()
+				{});
+				$("#iphone-inside").fadeOut("normal", function(){window.location="index.html";});
+			//});
+		}
+
 	});
+
 } else {
 	alert('What the f\u2730ck, no jQuery?');
 }
